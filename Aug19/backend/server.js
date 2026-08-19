801df6592c4b3e484d256077dda65b7437d9c909
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-19 17:21:10 +0600

    Added full path and regiter api is working

--- Aug19/backend/server.js
+++ Aug19/backend/server.js
@@ -3,10 +3,12 @@ const express = require("express");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
+const router = require("./Routes/userRoutes");
 
 const app = express();
+app.use(express.json());
 
-
+app.use("/api", router);
 
 connectDB();
 
